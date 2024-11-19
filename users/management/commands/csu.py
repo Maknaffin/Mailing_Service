dd1811e7e113b1029365f6b890d7bcdc46642bd5
Timofey <tima.khlystov99@mail.ru>
2024-11-19 22:43:45 +0300

    Initial commit

---
+++ users/management/commands/csu.py
@@ -0,0 +1,19 @@
+from asttokens.util import is_starred
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='admin@mail.ru',
+            first_name='Admin',
+            last_name='Admin',
+            is_staff=True,
+            is_superuser=True,
+        )
+
+        user.set_password('Qwerty1234')
+        user.save()
