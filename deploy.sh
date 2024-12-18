bc44443c9040cecb47b62be0f2bc518bbb88b73b
Timofey <tima.khlystov99@mail.ru>
2024-12-19 01:38:49 +0300

    fix cd

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
-python3 -m venv env
-source env/bin/activate
-pip3 install -r requirements.txt
-python3 manage.py migrate
-python3 manage.py collectstatic --no-input
-deactivate
+#python3 -m venv env
+#source env/bin/activate
+#pip3 install -r requirements.txt
+#python3 manage.py migrate
+#python3 manage.py collectstatic --no-input
+#deactivate
+docker-compose up --build -d
+docker-compose exec app python manage.py migrate
